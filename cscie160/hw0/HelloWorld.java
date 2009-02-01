77b7d4834ef197c532bae92c4e0b3cb7f97537b3
Harvard <bketelsen@gmail.com>
2009-02-01 12:39:42 -0500

    homework 0 complete

---
+++ cscie160/hw0/HelloWorld.java
@@ -0,0 +1,10 @@
+ package cscie160.hw0;
+ 
+   // File HelloWorld.java
+   public class HelloWorld
+   {
+      public static void main(String argv[])
+      {
+         System.out.println("Hello, Brave New World of Java!");
+      }
+   }
