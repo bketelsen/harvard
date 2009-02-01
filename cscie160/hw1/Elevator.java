0db44610667e21142d79c73f6cd36527940dd560
Harvard <bketelsen@gmail.com>
2009-02-01 14:25:53 -0500

    more

--- cscie160/hw1/Elevator.java
+++ cscie160/hw1/Elevator.java
@@ -10,61 +10,75 @@ public class Elevator {
     public static final int MAXIMUM_CAPACITY = 3;
     public static final int NUMBER_OF_FLOORS = 7;
 
-    enum DirectionOfTravel {UP, DOWN, STOPPED}
+    enum DirectionOfTravel {
+        UP, DOWN, STOPPED
+    }
 
 
-    // I think the next thing to do here is put in a "NEXT FLOOR" method that takes a direction and returns the
-    // next floor
     public enum Floor {
-    FIRST (1, "FIRST"),
-    SECOND   (2, "SECOND"),
-    THIRD   (3, "THIRD"),
-    FOURTH    (4, "FOURTH"),
-    FIFTH (5,   "FIFTH"),
-    SIXTH  (6, "SIXTH"),
-    SEVENTH  (7, "SEVENTH");
+        FIRST(1, "FIRST"),
+        SECOND(2, "SECOND"),
+        THIRD(3, "THIRD"),
+        FOURTH(4, "FOURTH"),
+        FIFTH(5, "FIFTH"),
+        SIXTH(6, "SIXTH"),
+        SEVENTH(7, "SEVENTH");
 
-    private final int number;
-    private final String name;
+        private final int number;
+        private final String name;
 
-    Floor(int number, String name) {
-        this.number = number;
-        this.name = name;
+        Floor(int number, String name) {
+            this.number = number;
+            this.name = name;
+        }
+
+        public int floorNumber() {
+            return number;
+        }
+
+        public String floorName() {
+            return name;
+        }
+
+        public Floor nextFloor(DirectionOfTravel dot) {
+            int floorOperand;
+            if (dot == DirectionOfTravel.UP) {
+                floorOperand = 1;
+            } else {
+                floorOperand = -1;
+            }
+
+            
+
+            return floor;
+        }
+
     }
-    public int floornumber()   { return number; }
-    public String floorname() { return name; }
-
-    }
 
 
-    private DirectionOfTravel _directionOfTravel; 
+    private DirectionOfTravel _directionOfTravel;
     private Floor _currentFloor;
     private int _passengersOnboard;
     private java.util.HashMap _floorQueue;
-    
 
-    
-    Elevator()
-    {
+
+    Elevator() {
         _currentFloor = Floor.FIRST;
         _passengersOnboard = 0;
         _directionOfTravel = DirectionOfTravel.STOPPED;
-        
+
     }
 
-    void move()
-    {
-        
+    void move() {
+
     }
 
-    void stop()
-    {
-   
+    void stop() {
+
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         StringBuilder output = new StringBuilder();
 
         output.append("Currently " + _passengersOnboard + " Passengers Onboard\r\n");
@@ -74,10 +88,9 @@ public class Elevator {
 
     }
 
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
         Elevator theElevator = new Elevator();
         System.out.println(theElevator.toString());
     }
-    
+
 }
